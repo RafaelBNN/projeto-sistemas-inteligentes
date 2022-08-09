54f6e759bfff5d1a710e75803a56ad039432d967
RafaelBNN <rafaelbnneto@gmail.com>
2022-08-09 09:08:26 -0300

    Random movement

--- agent.js
+++ agent.js
@@ -1,12 +1,36 @@
+let previous;
+
 class Agent{
-    constructor(x, y){
+    constructor(x=width/2 + GRID_SIZE/2, y= height/2 + GRID_SIZE/2){
         this.position = createVector(x, y);
-        this.size = 8;
+        this.size = GRID_SIZE - 2;
     }
 
     run(){
         stroke(0);
         fill(255,255,0);
         circle(this.position.x, this.position.y, this.size);
+        //this.keyPressed().keyCode = null;
+        this.wander();
+    }
+
+    wander() {
+
+        let qui = floor(random(0,10));
+
+        if (qui === 0 && previous != 1) {
+            this.position = createVector(this.position.x - GRID_SIZE, this.position.y);
+        } else if (qui === 1 && previous != 0) {
+            this.position = createVector(this.position.x + GRID_SIZE, this.position.y);
+        } else if (qui === 2 && previous != 3){
+            this.position = createVector(this.position.x, this.position.y - GRID_SIZE);
+        } else if (qui === 3 && previous != 2){
+            this.position = createVector(this.position.x, this.position.y + GRID_SIZE);
+        } 
+
+        if(qui < 4){
+            previous = qui;
+        }
+    
     }
 }
--- sketch.js
+++ sketch.js
@@ -5,50 +5,31 @@ let agent;
 
 const width = 1000;
 const height = 800;
+const GRID_SIZE = 10;
 
 function setup() {
   createCanvas(width, height); // cria o canvas
   generateMap(); // gera o grid do mapa
-  agent = new Agent(25, 25); // cria o agente
-  world = new World(20);
+  agent = new Agent(); // cria o agente
+  //world = new World(20);
 }
 
 function draw() {
   background(175); // limpa o background
   generateMap(); // gera o grid do mapa
-  world.run();
+  //world.run();
   agent.run();
 }
 
 function generateMap(){
 
-  for (let i = 5; i < width; i+=10) {
-    for (let j = 5; j < height; j+=10) {
+  rectMode(CORNER);
+
+  for (let i = 0; i < width; i+=GRID_SIZE) {
+    for (let j = 0; j < height; j+=GRID_SIZE) {
       stroke(0);
       fill(175);
-      rect(i, j, 10, 10);
+      rect(i, j, GRID_SIZE, GRID_SIZE);
     }
   }
-}
-
-function keyPressed() {
-
-  if (keyCode === LEFT_ARROW) {
-
-    agent.position = createVector(agent.position.x - 10, agent.position.y);
-
-  } else if (keyCode === RIGHT_ARROW) {
-
-    agent.position = createVector(agent.position.x + 10, agent.position.y);
-
-  } else if (keyCode === UP_ARROW){
-
-    agent.position = createVector(agent.position.x, agent.position.y - 10);
-
-  } else if (keyCode === DOWN_ARROW){
-
-    agent.position = createVector(agent.position.x, agent.position.y + 10);
-
-  } 
-
 }
